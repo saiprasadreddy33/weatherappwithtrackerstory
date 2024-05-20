2052287e84aa22c08ff9210e2da538a60d90b4ea
Sai Prasad Reddy <74254400+saiprasadreddy33@users.noreply.github.com>
2024-05-20 12:32:56 +0530

    Updated MoodTracker.js, Chart Updates

--- src/components/MoodTracker.js
+++ src/components/MoodTracker.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Line } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+import { useDispatch, useSelector } from 'react-redux';
+import { addMood } from '../slices/moodSlice';
+import './MoodTracker.css';
 
 const MoodTracker = () => {
   const [mood, setMood] = useState('');
-  const [moods, setMoods] = useState([]);
+  const moods = useSelector((state) => state.mood.moods);
+  const dispatch = useDispatch();
   const chartRef = useRef();
 
   const handleMoodChange = (event) => {
@@ -12,18 +15,20 @@ const MoodTracker = () => {
   };
 
   const handleMoodSubmit = () => {
-    setMoods([...moods, { mood, date: new Date().toLocaleDateString() }]);
-    setMood('');
+    if (mood) {
+      dispatch(addMood({ mood, date: new Date().toLocaleDateString() }));
+      setMood('');
+    }
   };
 
   useEffect(() => {
-    if (chartRef.current && chartRef.current.chartInstance) {
-      chartRef.current.chartInstance.destroy();
+    if (chartRef.current) {
+      chartRef.current.destroy();
     }
   }, [moods]);
 
-  const labels = moods.map(entry => entry.date);
-  const moodData = moods.map(entry => entry.mood);
+  const labels = moods.map((entry) => entry.date);
+  const moodData = moods.map((entry) => entry.mood);
 
   const data = {
     labels: labels,
@@ -40,7 +45,7 @@ const MoodTracker = () => {
   const chartOptions = {
     maintainAspectRatio: false,
     responsive: true,
-    aspectRatio: 1, // Set the aspect ratio for a square chart
+    aspectRatio: 1,
   };
 
   return (
@@ -53,8 +58,7 @@ const MoodTracker = () => {
         placeholder="Enter your mood"
       />
       <button onClick={handleMoodSubmit}>Submit</button>
-      <div style={{ width: '300px', height: '300px' }}> {/* Adjust the size as needed */}
-        <canvas id="myChart" width="80" height="0"></canvas> {/* Adjust the dimensions */}
+      <div style={{ width: '300px', height: '300px' }}>
         <Line data={data} options={chartOptions} ref={chartRef} />
       </div>
     </div>
